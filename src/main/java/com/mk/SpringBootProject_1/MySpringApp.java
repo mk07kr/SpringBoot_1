ca8887d1614acea4108d98ded32109f181a1118a
mk07kr <mk1309kr@gmail.com>
2025-01-15 15:37:14 +0530

    Transactional Added to SpringBoot Save Method

--- src/main/java/com/mk/SpringBootProject_1/MySpringApp.java
+++ src/main/java/com/mk/SpringBootProject_1/MySpringApp.java
@@ -2,12 +2,22 @@ package com.mk.SpringBootProject_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class MySpringApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringApp.class, args);
 	}
 
+	@Bean
+	public PlatformTransactionManager transManager(MongoDatabaseFactory dbFactory) {
+		return new MongoTransactionManager(dbFactory);
+	}
 }
--- src/main/java/com/mk/SpringBootProject_1/Service/JournalEntryService.java
+++ src/main/java/com/mk/SpringBootProject_1/Service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.mk.SpringBootProject_1.Repository.EntryRepo;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,14 +24,17 @@ public class JournalEntryService {
     public void save(JournalEntry journalEntry) {
        entryRepo.save(journalEntry);
     }
-
+    @Transactional
     public void save(JournalEntry journalEntry, String userName) {
-        Users user = UserService.findByUsername(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = entryRepo.save(journalEntry);
-    user.getEntries().add(saved);
-    UserService.save(user);
-
+        try {
+            Users user = UserService.findByUsername(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = entryRepo.save(journalEntry);
+            user.getEntries().add(saved);
+            UserService.save(user);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public List<JournalEntry> findAll() {
@@ -39,10 +43,16 @@ public class JournalEntryService {
     public Optional<JournalEntry> findById(ObjectId id) {
         return entryRepo.findById(id);
     }
+
     public void deleteById(ObjectId id, String userName) {
-        Users user = UserService.findByUsername(userName);
-        user.getEntries().removeIf(entry -> entry.getId().equals(id));
-        UserService.save(user);
-        entryRepo.deleteById(id);
+        try {
+            Users user = UserService.findByUsername(userName);
+            user.getEntries().removeIf(entry -> entry.getId().equals(id));
+            UserService.save(user);
+            entryRepo.deleteById(id);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
