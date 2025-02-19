16853b021cd0a2ecd108019a8ca03164c0301d13
mk07kr <mk1309kr@gmail.com>
2025-02-19 16:19:58 +0530

    SMTP Enabled success

--- src/main/java/com/mk/SpringBootProject_1/Service/EmailService.java
+++ src/main/java/com/mk/SpringBootProject_1/Service/EmailService.java
@@ -1,9 +1,27 @@
 package com.mk.SpringBootProject_1.Service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
 
+    public void sendEmail(String to, String subject, String body) {
+        try{
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(body);
+            mailSender.send(mail);
+        }
+        catch(Exception e){
+           log.error("Error sending email", e);
+        }
 
-}
+}}
